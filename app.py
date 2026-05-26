b8a8dd8bacd7bf8ccc9e443fd60ad21f81863f3b
elisifa-saidi <elisifasaidi@gmail.com>
2026-05-26 18:15:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -11,4 +11,8 @@ def submit_step3():
     return {"message": "POST WORKS"}
 
 if __name__ == "__main__":
-    app.run()
+    import os
+
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(host="0.0.0.0", port=port)
